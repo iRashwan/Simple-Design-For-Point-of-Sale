814de650038c2fa75a6e16700237953f7b4355ac
iRashwan <40560225+iRashwan@users.noreply.github.com>
2018-12-25 15:51:15 +0200

    Intial Project

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+// jQuery(document).ready(function() {
+
+
+//     $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
+//         $(this).removeClass('input-error');
+//     });
+
+//     $('.sign').on('click', function() {
+//         console.log("AHMED");
+//         if ($('#form-username').val() == 'admin' && $('#form-password').val() == '123') {
+//             console.log("essam");
+//             $('.loginCon').fadeOut();
+//         }
+//     });
+
+
+// });
